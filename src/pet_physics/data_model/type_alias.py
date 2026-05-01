cdc36f4c0348a60a5b1a96443eead9e47e146605
Florian Enzenhofer <>
2026-05-01 07:11:37 +0200

    feat: Add type alias definitions

---
+++ src/pet_physics/data_model/type_alias.py
@@ -0,0 +1,10 @@
+"""Contains type alias definitions."""
+
+RGBColor = tuple[float, float, float]
+"""A rgb color."""
+
+RGBAColor = tuple[float, float, float, float]
+"""A rgba color."""
+
+Size3d = tuple[int, int, int]
+"""A size in 3d."""
