62e7380246f825d94403677f8d12b6791662045d
Florian Enzenhofer <>
2026-08-03 21:15:48 +0200

    feat: add constants for simulation time and teleport interval

---
+++ src/pet_physics/constants.py
@@ -0,0 +1,6 @@
+"""Constants for the pet_physics package."""
+
+DEFAULT_SIM_END = 1200
+"""The default value of the total simulation time in seconds."""
+DEFAULT_TELEPORT_INTERVAL = 0.5
+"""The default time between the teleports of two bodies in seconds."""
