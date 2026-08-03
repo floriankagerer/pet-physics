5418f3a964892ef40744645c0efd2f5c054bdc04
Florian Enzenhofer <>
2026-08-03 21:14:59 +0200

    chore: remove obsolete test

--- tests/pet_physics/test_hello_world.py
+++
@@ -1,8 +0,0 @@
-"""Tests the module `hello_world`."""
-
-from pet_physics.hello_world import get_one
-
-
-def test_get_one() -> None:
-    """Tests the function get one."""
-    assert get_one() == 1
