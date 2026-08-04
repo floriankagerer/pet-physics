f663f6a616e4368c6162354b17e2aebaab183647
Florian Enzenhofer <>
2026-08-04 20:58:41 +0200

    feat: add custom type aliases for 3D and 4D vectors and positions

---
+++ src/pet_physics/type_alias_definition.py
@@ -0,0 +1,10 @@
+"""Contains custom type aliases used in the package."""
+
+Position3d = tuple[float, float, float]
+"""A type alias for a position in 3D space, represented as a tuple of three floats (x, y, z)."""
+Size3d = tuple[float, float, float]
+"""A type alias for a size in 3D space, represented as a tuple of three floats (length, width, height)."""
+Vector3d = tuple[float, float, float]
+"""A type alias for a 3D vector."""
+Vector4d = tuple[float, float, float, float]
+"""A type alias for a 4D vector."""
