b1d2410031fe547b4190a3664d4aa5e2d097e536
Florian Enzenhofer <>
2026-08-11 15:50:28 +0200

    feat: add constants that define the z-coordinate of the carrier's top side and the belowness threshold

--- src/pet_physics/constants.py
+++ src/pet_physics/constants.py
@@ -1,8 +1,13 @@
 """Constants for the pet_physics package."""
 
+BELOWNESS_THRESHOLD = 0.1
+"""This value defines the distance below the top side of the carrier such that a body is not considered as touching
+the floor and falling off a pallet, respectively."""
 DEFAULT_SIM_END = 1200
 """The default value of the total simulation time in seconds."""
 DEFAULT_TELEPORT_INTERVAL = 0.5
 """The default time between the teleports of two bodies in seconds."""
 UNIT_NORMAL_Z_AXIS = (0, 0, 1)
 """The unit normal that represents the direction of the z-axis."""
+Z_COORDINATE_TOP_SIDE_OF_CARRIER = 0.0
+"""The z-coordinate of the top side of the carrier."""
